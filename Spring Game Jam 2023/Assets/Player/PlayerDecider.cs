55a23098f5dea894c37e80f9181613b81a8edac2
LactiLad <rxcando@gmail.com>
2023-04-09 23:35:36 -0600

    Animations and FIXED netcode

--- Spring Game Jam 2023/Assets/Player/PlayerDecider.cs
+++ Spring Game Jam 2023/Assets/Player/PlayerDecider.cs
@@ -2,48 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
-
-public class PlayerDecider : NetworkBehaviour
-{
-    public bool day;
-    bool decided = false;
-    public Animator animator;
-    public AnimationClip[] dayDirectionAnimations; //0 idle, 1 up, 2 down, 3 left, 4 right
-    //public AnimationClip[] nightDirectionAnimations; //0 idle, 1 up, 2 down, 3 left, 4 right
-    // Start is called before the first frame update
-    void Awake()
+namespace QuickStart {
+    public class PlayerDecider : MonoBehaviour
     {
-        animator = GetComponent<Animator>();
-
-    }
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //find if day
-        int decisionDay = GameObject.Find("DecisionHolder").GetComponent<DecisionHolder>().day;
-        if (decisionDay != 0) {
-            if (decisionDay == 1)
-                day = true;
-            else
-                day = false;
+        static float lerpSpeed = 20f;
+        Vector2 direction;
+        public bool day;
+        bool decided = false;
+        public Animator animator;
+        // Start is called before the first frame update
+        void Start()
+        {
+            animator = GetComponent<Animator>();
         }
 
-        if (Input.GetAxis("Horizontal") > 0.1) {
-            //if (day) animator.
-        }
-        else if (Input.GetAxis("Horizontal") < -0.1) {
-            //right
-        }
-        else if (Input.GetAxis("Verticle") > 0.1) {
-            //up
-        }
-        else if (Input.GetAxis("Verticle") < -0.1) {
-            //down
+        // Update is called once per frame
+        void Update()
+        {
+            //find if day
+            int decisionDay = GameObject.Find("DecisionHolder").GetComponent<DecisionHolder>().day;
+            if (decisionDay != 0) {
+                if (decisionDay == 1)
+                    day = true;
+                else
+                    day = false;
+            }
+            direction = Vector2.Lerp(direction, GetComponent<PlayerScript>().avgMove, Time.deltaTime * lerpSpeed);
+            animator.SetBool("day", day);
+            animator.SetFloat("Horizontal", direction.x);
+            animator.SetFloat("Vertical", direction.y);
         }
     }
 }
